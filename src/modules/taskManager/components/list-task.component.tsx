4c4ea57eb1b7061b6f28a0a0e21779e2c658c2a2
Anderson Andrade <anderson_andrade_@outlook.com>
2024-02-23 01:26:27 -0300

    feat: remove buttons to action to submenu with the same actions

--- src/modules/taskManager/components/list-task.component.tsx
+++ src/modules/taskManager/components/list-task.component.tsx
@@ -4,49 +4,96 @@ import { Work } from '@/interfaces/task.interface';
 import { Alert } from '@/components/common/alert.component.tsx';
 import { Button } from '@/components/ui/button.tsx';
 import { CUSubTask } from '@/modules/taskManager/components/CU-sub-task.component.tsx';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu.tsx';
 
 interface ListTasksProps {
     work: Work;
 }
 
 export function ListTasks( { work }: ListTasksProps ) {
+
+    const formatDate = ( date: Date ): string => {
+        const newDate = new Date( date );
+
+        const dateFormatter = new Intl.DateTimeFormat( 'en-US', {
+            day: 'numeric',
+            month: 'short', // 'short' will give you the abbreviated month name
+            year: 'numeric',
+        } );
+
+        return dateFormatter.format( newDate );
+    };
+
     return (
         <>
-            <ul className="p-4">
+            <ul className="p-4 flex flex-col gap-2">
                 { work.tasks && work.tasks.map( task => {
                     return (
                         <li key={ task._id } id={ task._id }>
-                            <div className="flex flex-row gap-3 items-center">
-                                <span className="w-full">{ task.name }</span>
-                                <div className="flex flex-row justify-end gap-2 w-full">
-                                    <Alert
-                                        title={ `Alert, you are deleting` }
-                                        description={ `Do you want delete ${ task.name } ?`
-                                        }>
-                                        <Button><Trash2/></Button>
-                                    </Alert>
-                                    { !task.finished && <Button disabled={ !task.paused }><Play/></Button> }
-                                    { !task.finished && <Button disabled={ task.paused }><Pause/></Button> }
-                                    { !task.finished && <Button disabled={ task.finished }><CheckCheck/></Button> }
-                                    { !task.finished && <CUSubTask task={ task }></CUSubTask> }
+                            <div className="flex flex-col border-2 p-4 rounded-lg">
+                                <div className="flex flex-row gap-3 items-center justify-between">
+                                    <div className="w-full flex flex-row gap-2">
+                                        <span>{ task.name } - </span>
+                                        <span>{ formatDate( task.start ) }</span>
+                                        <span>{ task.finished ?? formatDate( task.finished ) }</span>
+                                    </div>
+                                    <div className="flex flex-row justify-end gap-2 w-full">
+                                        <Alert
+                                            title={ `Alert, you are deleting` }
+                                            description={ `Do you want delete ${ task.name } ?` }
+                                        >
+                                            <Button><Trash2/></Button>
+                                        </Alert>
+
+                                        <DropdownMenu>
+                                            <DropdownMenuTrigger asChild>
+                                                <Button>Action</Button>
+                                            </DropdownMenuTrigger>
+                                            <DropdownMenuContent>
+                                                <DropdownMenuGroup>
+                                                    <DropdownMenuItem className="flex flex-row gap-2">
+                                                        <Play/>
+                                                        Play
+                                                    </DropdownMenuItem>
+                                                    <DropdownMenuItem className="flex flex-row gap-2">
+                                                        <Pause/>
+                                                        Pause
+                                                    </DropdownMenuItem>
+                                                    <DropdownMenuItem className="flex flex-row gap-2">
+                                                        <CheckCheck/>
+                                                        Finish
+                                                    </DropdownMenuItem>
+                                                </DropdownMenuGroup>
+                                            </DropdownMenuContent>
+                                        </DropdownMenu>
+
+                                        { !task.finished && <CUSubTask task={ task }></CUSubTask> }
+                                    </div>
                                 </div>
+
+                                {/* list sub tasks */ }
+                                <ul className="p-4 flex flex-col gap-2">
+                                    { task.subTasks && task.subTasks.map( subTask => {
+                                        return (
+                                            <li key={ subTask._id } id={ subTask._id }>
+                                                <div className="flex flex-row items-center justify-between">
+                                                    { subTask.description }
+                                                    <Alert title={ `Alert, you are deleting a sub task` }
+                                                           description={ `Do you want delete ${ subTask.description } ?` }>
+                                                        <Button><Trash2/></Button>
+                                                    </Alert>
+                                                </div>
+                                            </li>
+                                        );
+                                    } ) }
+                                </ul>
                             </div>
-                            {/* list sub tasks */ }
-                            <ul className="p-4 flex flex-col gap-2">
-                                { task.subTasks && task.subTasks.map( subTask => {
-                                    return (
-                                        <li key={ subTask._id } id={ subTask._id }>
-                                            <div className="flex flex-row items-center justify-between">
-                                                { subTask.description }
-                                                <Alert title={ `Alert, you are deleting a sub task` }
-                                                       description={ `Do you want delete ${ subTask.description } ?` }>
-                                                    <Button><Trash2/></Button>
-                                                </Alert>
-                                            </div>
-                                        </li>
-                                    );
-                                } ) }
-                            </ul>
                         </li>
                     );
                 } ) }
