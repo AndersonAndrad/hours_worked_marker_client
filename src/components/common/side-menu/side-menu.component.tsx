87a1e9a4eeeb4d292df600abcee94e897fb2ce28
Anderson Andrade <anderson_andrade_@outlook.com>
2024-04-07 17:47:41 -0300

    refact: changing icons by side menu

--- src/components/common/side-menu/side-menu.component.tsx
+++ src/components/common/side-menu/side-menu.component.tsx
@@ -1,5 +1,5 @@
 import { Accordion, AccordionContent, AccordionTrigger } from "../../ui/accordion";
-import { FolderGit2, FolderRoot, Home, LayoutDashboard, LucideIcon } from "lucide-react";
+import { FileBarChart, FolderGit2, FolderRoot, HandCoins, Home, LayoutDashboard, LucideIcon, WalletCards } from "lucide-react";
 import { useEffect, useState } from "react";
 
 import { AccordionItem } from "@radix-ui/react-accordion";
@@ -30,6 +30,7 @@ export function SideMenu() {
   useEffect(() => {
     const items: MenuItem[] = [
       { label: 'Profile', path: '', disabled: true, icon: Home, parent: MenuAccordion.USER, selected: false, id: generateHash() },
+
       // Projects
       { label: 'Dashboard', path: '/project/dashboard', disabled: true, icon: LayoutDashboard, parent: MenuAccordion.PROJECTS, selected: false, id: generateHash() },
       { label: 'Projects', path: '/projects', disabled: false, icon: FolderRoot, parent: MenuAccordion.PROJECTS, selected: false, id: generateHash() },
@@ -37,7 +38,14 @@ export function SideMenu() {
 
       // Financy
       { label: 'Dashboard', path: '/financy/dashboard', disabled: true, icon: LayoutDashboard, parent: MenuAccordion.FINANCY, selected: false, id: generateHash() },
-      { label: 'Financy', path: '', disabled: true, icon: Home, parent: MenuAccordion.FINANCY, selected: false, id: generateHash() },
+      { label: 'Bills to pay', path: '', disabled: true, icon: Home, parent: MenuAccordion.FINANCY, selected: false, id: generateHash() },
+      { label: 'Bills to receive', path: '', disabled: true, icon: HandCoins, parent: MenuAccordion.FINANCY, selected: false, id: generateHash() },
+      { label: 'Category', path: '', disabled: true, icon: Home, parent: MenuAccordion.FINANCY, selected: false, id: generateHash() },
+      { label: 'Client', path: '', disabled: true, icon: Home, parent: MenuAccordion.FINANCY, selected: false, id: generateHash() },
+      { label: 'Account bank', path: '', disabled: true, icon: WalletCards, parent: MenuAccordion.FINANCY, selected: false, id: generateHash() },
+      { label: 'Cost center', path: '', disabled: true, icon: Home, parent: MenuAccordion.FINANCY, selected: false, id: generateHash() },
+      { label: 'Type payment', path: '', disabled: true, icon: Home, parent: MenuAccordion.FINANCY, selected: false, id: generateHash() },
+      { label: 'DRE', path: '', disabled: true, icon: FileBarChart, parent: MenuAccordion.FINANCY, selected: false, id: generateHash() },
     ]
     setMenuItems(items);
   }, [])
