9386f85c13d7f54c6fc57b8f5b7bd156e4844051
Anderson Andrade <anderson_andrade_@outlook.com>
2024-04-07 20:19:23 -0300

    chore: implementing docker to client project

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove the previous Docker image (if exists)
+docker image rm -f personal_erp_client-personal_client:latest	
+
+# Build the new Docker image
+docker-compose build
+
+# Start the Docker containers
+docker-compose up -d
