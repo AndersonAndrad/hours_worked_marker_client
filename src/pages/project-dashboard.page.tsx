431c584d83075ee11a845fe8baf5188234c2a49c
Anderson Andrade <anderson_andrade_@outlook.com>
2024-04-24 01:10:10 -0300

    feat: implementing a library to display charts
    missing pass real data and implementing filtering by charts

--- src/pages/project-dashboard.page.tsx
+++ src/pages/project-dashboard.page.tsx
@@ -1,8 +1,26 @@
+import { Bar, BarChart, CartesianGrid, Legend, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+
 import { Container } from "@/components/common/container.component";
 import { Main } from "@/components/common/main.component";
 import { SideFilterDashboard } from "@/components/dashboard/side-filter.component";
+import { faker } from '@faker-js/faker';
+import { useEffect } from 'react';
 
 export function ProjectDashboard() {
+  const data = [];
+
+  useEffect(() => {
+    Array.from({ length: 12 }).forEach((_, index) => {
+      data.push({
+        name: `${index + 1}`,
+        uv: faker.number.int({ max: 24 }),
+        pv: faker.number.int({ max: 24 }),
+        oc: faker.number.int({ max: 24 }),
+        amt: faker.number.int({ max: 24 })
+      })
+    })
+  }, [])
+
   return (
     <Container>
       <header className='flex flex-row items-center justify-between'>
@@ -10,9 +28,28 @@ export function ProjectDashboard() {
         <SideFilterDashboard />
       </header>
       <Main>
-        <div>
-          dashboard
-        </div>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+            <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+            <Bar dataKey="oc" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+          </BarChart>
+        </ResponsiveContainer>
       </Main>
     </Container>
   )
