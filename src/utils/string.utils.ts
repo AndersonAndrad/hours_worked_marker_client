76dde24946d0a29eb643001c0b67e2d2690ff1ec
Anderson Andrade <anderson_andrade_@outlook.com>
2024-05-05 23:42:00 -0300

    feat: implement a string utility function to remove all unnecessary white space

--- src/utils/string.utils.ts
+++ src/utils/string.utils.ts
@@ -1,3 +1,7 @@
 export const firstLetterUppercase = (word: string): string => {
   return word.charAt(0).toUpperCase() + word.slice(1);
 };
+
+export const removeUnecessaryWhiteSpace = (str: string): string => {
+  return str.replace(/\s+/g, ' ').trim();
+};
