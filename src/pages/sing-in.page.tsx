956f24c289cd9f35e8676eca49079f68202fefaf
Anderson Andrade <anderson_andrade_@outlook.com>
2024-05-24 00:09:28 -0300

    refact: implement use state to get all information

--- src/pages/sing-in.page.tsx
+++ src/pages/sing-in.page.tsx
@@ -10,8 +10,21 @@ enum TypeTab {
   singin = 'Singin'
 }
 
+/**
+ * @todo pass all use state to form
+ */
 export function AuthenticatePage() {
   const [seePassword, setSeePassword] = useState<boolean>(false);
+
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [nickName, setNickName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+
+  const submitSingin = () => { };
+
+  const submitLogin = () => { };
 
   return (
     <div className="w-full flex justify-center items-center">
@@ -23,28 +36,27 @@ export function AuthenticatePage() {
         <TabsContent value={TypeTab.singin} className="h-[600px]">
           <div className="flex flex-col gap-3">
             <div className="flex flex-col gap-2">
-              <label htmlFor="">Name</label>
-              <Input />
+              <Input onChange={(event => setName(event.target.value))} />
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="">Nick name</label>
-              <Input />
+              <Input onChange={(event => setNickName(event.target.value))} />
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="">Email</label>
-              <Input />
+              <Input onChange={(event => (setEmail(event.target.value)))} />
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="">Password</label>
               <div className="flex gap-2 items-center">
-                <Input type={seePassword ? 'text' : 'password'} />
+                <Input type={seePassword ? 'text' : 'password'} onChange={(event => setPassword(event.target.value))} />
                 <Button variant='secondary' onClick={() => setSeePassword(!seePassword)}>{seePassword ? <EyeOff /> : <Eye />}</Button>
               </div>
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="">Confirm Password</label>
               <div className="flex gap-2 items-center">
-                <Input type={seePassword ? 'text' : 'password'} />
+                <Input type={seePassword ? 'text' : 'password'} onChange={(event => setConfirmPassword(event.target.value))} />
                 <Button variant='secondary' onClick={() => setSeePassword(!seePassword)}>{seePassword ? <EyeOff /> : <Eye />}</Button>
               </div>
             </div>
@@ -58,12 +70,12 @@ export function AuthenticatePage() {
           <div className="flex flex-col gap-3">
             <div className="flex flex-col gap-2">
               <label htmlFor="">Email / Nick</label>
-              <Input />
+              <Input onChange={(event => (setEmail(event.target.value)))} />
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="">Password</label>
               <div className="flex gap-2 items-center">
-                <Input type={seePassword ? 'text' : 'password'} />
+                <Input type={seePassword ? 'text' : 'password'} onChange={(event => setPassword(event.target.value))} />
                 <Button variant='secondary' onClick={() => setSeePassword(!seePassword)}>{seePassword ? <EyeOff /> : <Eye />}</Button>
               </div>
             </div>
