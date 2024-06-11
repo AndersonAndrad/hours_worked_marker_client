79c9c47983c2264178aabd75daf9165d66a9a8f3
Anderson Andrade <anderson_andrade_@outlook.com>
2024-06-11 19:35:59 -0300

    refact: implements to show user email in the header

--- src/components/common/navbar.component.tsx
+++ src/components/common/navbar.component.tsx
@@ -9,7 +9,10 @@ export function Navbar() {
 
   return (
     <div className="flex flex-row justify-end items-center gap-5 p-2 border-b border-opacity-25 border-white">
-      <span>{user?.name}</span>
+      <div className="flex flex-col items-end">
+        <span>{user?.name}</span>
+        <span className="font-thin text-sm">{user?.email}</span>
+      </div>
 
       <AvatarIcon width='40' height='40' />
     </div>
